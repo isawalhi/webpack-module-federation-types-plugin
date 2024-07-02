627ff26d894594b064d5e473bd7d96e9bf298fe3
Ibrahim Sawalhi <ibrahim.sawalhi@superside.com>
2024-07-02 16:42:05 +0400

    chore: parse tsconfig.json with built-in ts helper

--- src/compileTypes/helpers/getTSConfigCompilerOptions.ts
+++ src/compileTypes/helpers/getTSConfigCompilerOptions.ts
@@ -1,17 +1,26 @@
-import path from 'path';
+import path from "path";
 
-import ts from 'typescript';
+import ts from "typescript";
 
-import { getLogger } from '../../helpers';
+import { getLogger } from "../../helpers";
 
-export function getTSConfigCompilerOptions(tsconfigFileNameOrPath: string): ts.CompilerOptions {
+export function getTSConfigCompilerOptions(
+  tsconfigFileNameOrPath: string
+): ts.CompilerOptions {
   const logger = getLogger();
 
   const tsconfigPath = path.resolve(tsconfigFileNameOrPath);
   if (!tsconfigPath) {
-    logger.error('ERROR: Could not find a valid tsconfig.json');
+    logger.error("ERROR: Could not find a valid tsconfig.json");
     process.exit(1);
   }
 
-  return require(tsconfigPath).compilerOptions;
+  const tsconfigJsonFile = ts.readJsonConfigFile(tsconfigPath, ts.sys.readFile);
+  const parsedConfig = ts.parseJsonSourceFileConfigFileContent(
+    tsconfigJsonFile,
+    ts.sys,
+    path.dirname(tsconfigPath)
+  );
+
+  return parsedConfig.options;
 }
